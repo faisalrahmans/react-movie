05ee1e331afcc81abfb2b69dac276be24538869c
faisalrahman <faisalrahsu@gmail.com>
2021-07-08 13:50:42 +0700

    create movie page and header for searching movies

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Movies from './Component/Movies/Movies'
+
+// const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=ecd2dc88'
+// const api_key = 'faf7e5bb'
+
+// const api = axios.create({ baseURL: BASE_URL });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movies />
     </div>
   );
 }
